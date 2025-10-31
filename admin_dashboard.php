fc7f3290cc7f2f7e3c4bbf5603806d6a74a761ad
Istifanus Alexander1 <istifanusalexander@gmail.com>
2025-10-31 14:49:32 +0000

    new Update cal

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -2,23 +2,48 @@
     session_start();
 ?>
 
-<?php
-
-    echo $_SESSION['userName'];
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome Admin</title>
+    <link rel="stylesheet" href="admin_calculator.css">
 </head>
 <body>
-    <h1>Hi, Welcome <?php echo $_SESSION['userName']; ?> This is Admin Dashboard!</h1>
-    <form action="admin_dashboard.php" method="post">
-        <button type="submit" name="logout" onclick="confirm('Are you sure you want to exit?')">Logout</button>
-    </form>
+    <nav>
+        <h1>Hi <?php echo $_SESSION['userName']; ?>, Welcome This is Admin Dashboard!</h1>
+        <form action="admin_dashboard.php" method="post">
+            <div class="div-logout-btn">
+                <button class="logout-btn" type="submit" name="logout" onclick="confirm('Are you sure you want to exit?')">Logout</button>
+            </div>
+        </form>
+    </nav>
+
+    <div class="calculator">
+        <h1>Calculator</h1>
+        <div class="main-calculator">
+            <input type="text" readonly>
+            <div class="keys">
+                <button onclick="appendToDisplay('+')">+</button>
+                <button onclick="appendToDisplay('7')">7</button>
+                <button onclick="appendToDisplay('8')">8</button>
+                <button onclick="appendToDisplay('9')">9</button>
+                <button onclick="appendToDisplay('-')">-</button>
+                <button onclick="appendToDisplay('4')">4</button>
+                <button onclick="appendToDisplay('5')">5</button>
+                <button onclick="appendToDisplay('6')">6</button>
+                <button onclick="appendToDisplay('*')">*</button>
+                <button onclick="appendToDisplay('1')">1</button>
+                <button onclick="appendToDisplay('2')">2</button>
+                <button onclick="appendToDisplay('3')">3</button>
+                <button onclick="appendToDisplay('.')">.</button>
+                <button onclick="appendToDisplay('0')">0</button>
+                <button onclick="displayResault()">=</button>
+                <button onclick="clearScreen()">C</button>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
 
